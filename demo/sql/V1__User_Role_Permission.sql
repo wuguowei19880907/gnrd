1829c40ff331fbc6b90d30ec8819623492d6594a
wuguowei <wuguowei@ydxsafe.com>
2024-06-27 15:12:39 +0800

    添加用户角色权限表

---
+++ demo/sql/V1__User_Role_Permission.sql
@@ -0,0 +1,82 @@
+create table c_user
+(
+    id         bigint
+        constraint c_user_pk primary key
+        constraint id_gt_zero check ( id > 0 ),
+    c_name     varchar(255) not null unique ,
+    c_phone    varchar(20) unique check ( length(c_phone)=11 ),
+    c_password varchar(255),
+    c_state    smallint not null
+        constraint state_in check ( c_state = 0 or c_user.c_state = 1 ),
+    create_time timestamp
+);
+comment on column c_user.id is '主键';
+comment on column c_user.c_name is '用户名';
+comment on column c_user.c_phone is '手机号';
+comment on column c_user.c_phone is '手机号';
+comment on column c_user.c_password is '登录密码';
+comment on column c_user.c_state is '用户状态；0-禁用 1-启用';
+comment on column c_user.create_time is '用户创建时间';
+
+create table c_role
+(
+    id bigint constraint c_role_pk primary key
+        constraint id_gt_zero check ( id > 0 ),
+    c_name varchar(255) not null unique,
+    c_code varchar(255) not null unique,
+    c_state    smallint not null default 1
+        constraint state_in check ( c_state = 0 or c_role.c_state = 1 ),
+    create_time timestamp
+);
+
+comment on column c_role.id is '主键';
+comment on column c_role.c_name is '角色名';
+comment on column c_role.c_code is '角色编码';
+comment on column c_role.c_state is '状态；0-禁用 1-启用';
+comment on column c_role.create_time is '创建时间';
+
+create table c_user_role
+(
+    id bigint constraint c_user_role_pk primary key
+        constraint id_gt_zero check ( id > 0 ),
+    c_user_id bigint not null,
+    c_role_id bigint not null,
+    create_time timestamp
+);
+
+comment on column c_user_role.id is '主键';
+comment on column c_user_role.c_user_id is '用户id';
+comment on column c_user_role.c_role_id is '角色id';
+comment on column c_role.create_time is '创建时间';
+
+create table c_permission
+(
+    id bigint constraint c_permission_pk primary key
+        constraint id_gt_zero check ( id > 0 ),
+    c_name varchar(255) not null unique,
+    c_code varchar(255) not null unique,
+    c_state    smallint not null default 1
+        constraint state_in check ( c_state = 0 or c_permission.c_state = 1 ),
+    create_time timestamp
+);
+
+comment on column c_permission.id is '主键';
+comment on column c_permission.c_name is '权限名';
+comment on column c_permission.c_code is '权限编码';
+comment on column c_permission.c_state is '状态；0-禁用 1-启用';
+comment on column c_permission.create_time is '创建时间';
+
+create table c_role_permission
+(
+    id bigint constraint c_role_permission_pk primary key
+        constraint id_gt_zero check ( id > 0 ),
+    c_role_id bigint not null,
+    c_permission_id bigint not null,
+    create_time timestamp
+);
+
+comment on column c_role_permission.id is '主键';
+comment on column c_role_permission.c_role_id is '角色id';
+comment on column c_role_permission.c_permission_id is '权限id';
+comment on column c_role_permission.create_time is '创建时间';
+
---
+++ demo/src/main/java/org/gnrd/lam/entity/UserPO.java
@@ -0,0 +1,44 @@
+package org.gnrd.lam.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "c_user")
+public class UserPO extends BasePO {
+
+    /**
+     * 用户名称
+     */
+    @Column(name = "c_name")
+    private String name;
+
+    /**
+     * 手机号
+     */
+    @Column(name = "c_phone")
+    private String phone;
+
+    /**
+     * 登录密码
+     */
+    @Column(name = "c_password")
+    private String password;
+
+    /**
+     * 状态 0-禁用 1-启用
+     */
+    @Column(name = "c_state")
+    private Integer state;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+}
